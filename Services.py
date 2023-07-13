2e3ee944dab9d49a5ba0232ef994d093cfbabae7
Vikash-Data-Analytics <136671732+Vikash-Data-Analytics@users.noreply.github.com>
2023-07-13 17:45:31 +0530

    Update Services.py

--- Services.py
+++ Services.py
@@ -22,16 +22,16 @@ st.set_page_config(
     # layout = 'wide',
 )
 
-streamlit_style = """
-			<style>
-			@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+# streamlit_style = """
+# 			<style>
+# 			@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
 
-			html, body, [class*="css"]  {
-			font-family: 'Roboto', sans-serif;
-			}
-			</style>
-			"""
-st.markdown(streamlit_style, unsafe_allow_html=True)
+# 			html, body, [class*="css"]  {
+# 			font-family: 'Roboto', sans-serif;
+# 			}
+# 			</style>
+# 			"""
+# st.markdown(streamlit_style, unsafe_allow_html=True)
 hide_streamlit_style = """
     <style>
         #MainMenu {visibility: hidden;}
